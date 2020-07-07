c8133ebb06a7fb9bce863743a475d56bab0d14c8
James Ma <james@pop-os.localdomain>
2020-07-07 14:31:11 -0700

    added multithreading and multi link support

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
 # ebay-view-bot
-The simplest possible requests bot to boost ebay view count. Simply input desired product link (no cached links plz) and desired view count and voila.
+A bot to boost viewer count for eBay listings. Simply setup a links.py file with the links and desired view count.
+
+Forked from [washedgram/ebay-view-bot](https://github.com/washedgram/ebay-view-bot) and heavily improved upon.
 
 ## TODO
-- [ ] txt file for all desired links
-- [ ] start on a new link after one finishes instead of finishing script
-- [ ] multithreading
 - [ ] proxy support
 
 ## Installation
@@ -14,8 +13,34 @@ The simplest possible requests bot to boost ebay view count. Simply input desire
 ## What do i need to run this?
 Python, Requests
 
-- [Install Python 3.x.x](https://www.python.org/downloads/). If you have it installed correctly, typing 'python3' in your Terminal will open an interactive console where you can execute Python code. 
+- [Install Python 3.x.x](https://www.python.org/downloads/). If you have it installed correctly, typing 'python3' in your Terminal will open an interactive console where you can execute Python code.
 - Install Requests: `pip install requests` - need to install python first
+
+## Setup
+
+- Make a file called `links.py` and edit the file with a text editor:
+
+```
+# links.py
+links_to_views = {
+    "<link 1>": <number of views for link 1>,
+    "<link 2>": <number of views for link 2>,
+    # ...
+    # ... (continue with this format for all listings)
+    # ...
+    "<last link>": <number of views for last link> # (no comma for last listing)
+}
+```
+
+- An example `links.py` file:
+
+```
+# links.py
+links_to_views = {
+    "https://www.ebay.com/itm/Supreme-Raffia-Tote-Bag-Red-SS20-IN-HAND-DEADSTOCK/303616402396?hash=item46b0f2a3dc:g:pwMAAOSwPU1fA-5q": 5,
+    "https://www.ebay.com/itm/Supreme-Raffia-tote-Red-Brand-new/383621615751?hash=item5951a15087:g:JYAAAOSwW1Fe-1bd": 5,
+}
+```
 
 ## Execution
 
--- main.py
+++ main.py
@@ -1,36 +1,66 @@
+import multiprocessing
 import requests
 import time
+from links import links_to_views
 
-def view():
-    print
-    print (" --------------------------------------------")
-    print (" viewbot v.1.0.0")
-    print (" --------------------------------------------\n")
-    listingURL = input(" Link : ")
-    print
-    # viewCount = int(raw_input(" How many views? : "))
-    viewCount = int(input(" How many views? : "))
-
-    print
-    print (" Watching ... ")
-    print (" Do not close this window. ")
-
+def view( link, views ):
     print
     start_time = time.time()
-    for i in range(viewCount):
-        r = requests.get(listingURL)
-    
-    print (" Task completed! ")
-    viewTime = float(time.time() - start_time)
-    print(" Total time : " + " %s sec" % viewTime)
-    viewRate = float(viewCount / viewTime)
-    print (" View rate  : " +  " %s views/sec" % viewRate)
-    print
-    print
-    
-    # todo : iterate through other links in txt file, multithread, proxy support
+    for i in range(views):
+        r = requests.get(link)
 
+    print ("     {} completed!".format(link))
+    print ("     {} views in :".format(views))
+    view_time = float(time.time() - start_time)
+    print ("     Time      : " + " %s sec" % view_time)
+    view_rate = float(views / view_time)
+    print ("     View rate : " +  " %s views/sec" % view_rate)
+    print ()
+
+    # todo : proxy support
+
+def intro( links_to_views_dict ):
+    print ("\n --------------------------------------------")
+    print (" viewbot v.2.0.0")
+    print (" --------------------------------------------\n")
+    # Listing the links and views
+    print (" Links ({}) :\n".format(len(links_to_views_dict)))
+    for link, views in links_to_views_dict.items():
+        print(" - {} - {} views".format(link, views))
+
+def view_all( links_to_views_dict ):
+    print ("\n --------------------------------------------")
+    print (" Watching ... ")
+    print (" Do not close this window. \n")
+
+    jobs = []
+
+    # making the multiprocessing jobs
+    for link, views in links_to_views_dict.items():
+        jobs.append(multiprocessing.Process(target=view, args=(link, views, )))
+
+    # doing the multiprocessing jobs
+    start_time = time.time()
+    for job in jobs:
+        job.start()
+
+    # wait for all jobs to finish
+    for job in jobs:
+        job.join()
+
+    total_views = 0
+
+    for views in links_to_views_dict.values():
+        total_views += views
+
+    print (" Tasks completed! ")
+    total_view_time = float(time.time() - start_time)
+    print (" Total time : " + " %s sec" % total_view_time)
+    view_rate = float(total_views / total_view_time)
+    print (" View rate  : " +  " %s views/sec" % view_rate)
+    print
+    print
 
 if __name__ == '__main__':
-  view()
-
+  intro( links_to_views )
+  view_all( links_to_views )

749ab3937bedb73c042c66f4a7f1e27b7b03409f
James Ma <james@pop-os.localdomain>
2020-07-07 14:33:41 -0700

    added .gitignore

--- links.py
+++
@@ -1,5 +0,0 @@
-# links.py
-links_to_views = {
-    "https://www.ebay.com/itm/Supreme-Raffia-Tote-Bag-Red-SS20-IN-HAND-DEADSTOCK/303616402396?hash=item46b0f2a3dc:g:pwMAAOSwPU1fA-5q": 1,
-    "https://www.ebay.com/itm/Supreme-Raffia-tote-Red-Brand-new/383621615751?hash=item5951a15087:g:JYAAAOSwW1Fe-1bd": 5,
-}
